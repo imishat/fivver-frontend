10a70ebabdd43ea455bd91d2241e6d237c9db0be
Arman <arman@localhost.localdomain>
2023-11-23 11:24:30 -0500

    deploy

--- next.config.js
+++ next.config.js
@@ -2,7 +2,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
+  output: 'export',
+  images: {
+    loader: 'custom',
+    loaderFile: './my-loader.ts',
+  },
   images: {
     remotePatterns: [
       {
