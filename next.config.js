80ac225efaf51ca560ed5c5b2bdfd6a4e02e6d03
Arman Khan <sparmankhan@gmail.com>
2023-11-24 13:09:36 -0500

    update something

--- next.config.js
+++ next.config.js
@@ -2,12 +2,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
   images: {
-    loader: 'custom',
-    loaderFile: './my-loader.ts',
-  },
-  images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'http',
